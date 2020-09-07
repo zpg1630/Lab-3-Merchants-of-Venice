7b65d043938f3d3740b2fd786cbc97d44e1c7f31
zpg1630 <zpg1630@g.rit.edu>
2020-09-06 23:45:38 -0400

    Holy cow, finished just in time

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import merchants
-import sys
+from typing import List
+from typing import Tuple
 
 
 def main():
@@ -7,6 +8,42 @@ def main():
     merchant = merchants.read_merchant(filename)
     for i in range(len(merchant)):
         print(merchant[i])
+    sort_merch = quick_sort(merchant)
+    print(len(sort_merch)//2)
+
+
+def _partition(data: List[merchants.Merchant], pivot: int) \
+      -> Tuple[List[merchants.Merchant], List[merchants.Merchant], List[merchants.Merchant]]:
+    """
+    Three way partition the data into smaller, equal and greater lists,
+    in relationship to the pivot
+    :param data: The data to be sorted (a list)
+    :param pivot: The value to partition the data on
+    :return: Three list: smaller, equal and greater
+    """
+    less, equal, greater = [], [], []
+    for element in data:
+        if element.location < pivot:
+            less.append(element)
+        elif element.location > pivot:
+            greater.append(element)
+        else:
+            equal.append(element)
+    return less, equal, greater
+
+
+def quick_sort(data: List[merchants.Merchant]) -> List[merchants.Merchant]:
+    """
+    Performs a quick sort and returns a newly sorted list
+    :param data: The data to be sorted (a list)
+    :return: A sorted list
+    """
+    if len(data) == 0:
+        return []
+    else:
+        pivot = data[0].location
+        less, equal, greater = _partition(data, pivot)
+        return quick_sort(less) + equal + quick_sort(greater)
 
 
 if __name__ == '__main__':
