93068d9962ce3ada71b1fe4f3591f0544da035e0
zpg1630 <zpg1630@g.rit.edu>
2020-09-06 23:08:12 -0400

    started inLab

---
+++ src/merchants.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Merchant:
+    name: str
+    location: int
+
+
+def read_merchant(filename: str) -> List[Merchant]:
+    """
+    Read merchants from a file into a list of Merchant dataclass objects.
+    :param filename: The name of the file
+    :return: A list of Merchants
+    """
+    merchants = list()
+    with open(filename) as f:
+        for line in f:
+            fields = line.split(',')
+            merchants.append(Merchant(
+                name=fields[1],
+                location=int(fields[0]),
+            ))
+    print(merchants)
+    return merchants
